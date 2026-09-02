7ba566b2cd0b3fd65c3119790180b34bbaf5c42b
Donverdict <e0094432@gmail.com>
2026-09-02 14:38:05 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -650,7 +650,7 @@ function exportCSV() {
       r.work_start||'', r.break_time||''
     ].join(';'));
   });
-  const blob = new Blob(["\uFEFF" + lines.join('\n')], {type: 'text/csv;charset=utf-8;'});
+  const blob = new Blob(["\\uFEFF" + lines.join('\\n')], {type: 'text/csv;charset=utf-8;'});
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
